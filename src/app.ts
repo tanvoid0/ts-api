f1233c2e93d529e3f8e023869883198ca5eb2106
Tan <thaque20@gmail.com>
2021-05-01 15:25:48 +0100

    Deploy 1.0.2

---
+++ src/app.ts
@@ -0,0 +1,82 @@
+import express from "express";
+import compression from 'compression';
+import cors from 'cors';
+import * as dotenv from "dotenv";
+import session from 'express-session';
+import cookieParser from 'cookie-parser';
+import passport from 'passport';
+
+// import db from "./config/db.config";
+// import {router as GameRouter} from './api/game/game.routes';
+// import {router as AuthRouter} from './api/auth/auth.routes';
+// import {router as ViewRouter} from './views/routes';
+// import AuthService from "./api/auth/auth.service";
+
+dotenv.config();
+
+class App {
+    public app: express.Application;
+
+    constructor() {
+        this.app = express();
+        this.config();
+        this.middleware();
+        this.routes();
+    }
+
+    private middleware(): void {
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({extended: true}));
+        this.app.use(cors());
+        // AuthService.config();
+    }
+
+    private routes(): void {
+        this.app.get("/", (req, res) => {
+            res.send("<h1>Welcome to your simple server! Awesome right</h1>");
+        });
+
+        // this.app.use('/views', ViewRouter);
+        // this.app.use('/api/game', GameRouter);
+        // this.app.use('/api/auth', AuthRouter);
+
+        // this.app.use("*", (req, res, next) => {
+        //     res.send("Make sure url is correct!!!");
+        // });
+    }
+
+    public config(): void {
+        const port = process.env.PORT || 3070;
+        const secret = process.env.JWT_TOKEN || "secretcode";
+
+        // db.connect();
+
+        // this.app.set('views', __dirname + '\\views');
+        // this.app.set('view engine', 'jsx');
+        // this.app.engine('jsx', require('express-react-views').createEngine({beautify: true}));
+
+        this.app.set("port", port);
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({extended: false}));
+        this.app.use(compression());
+        this.app.use(cors());
+        this.app.use(session({
+            secret: secret,
+            resave: true,
+            saveUninitialized: true,
+        }));
+        this.app.use(cookieParser());
+        this.app.use(passport.initialize());
+        this.app.use(passport.session());
+    }
+
+    public start(): void {
+        this.app.listen(this.app.get("port"), () => {
+            console.log(`API is running at http://localhost:${this.app.get('port')}`);
+        });
+    }
+}
+
+const app = new App();
+
+export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
-import express from 'express';
-const app = express()
+import App from './app';
 
-const PORT : string|number = process.env.PORT || 5000;
+require('dotenv').config();
 
-app.use("*",(req, res) =>{
-    res.send("<h1>Welcome to your simple server! Awesome right</h1>");
-});
+App.start();
 
-app.listen(PORT,() => console.log(`hosting @${PORT}`));
+module.exports = App;
+
+// import express from 'express';
+// const app = express()
+//
+// const PORT : string|number = process.env.PORT || 5000;
+//
+// app.use("*",(req, res) =>{
+//     res.send("<h1>Welcome to your simple server! Awesome right</h1>");
+// });
+//
+// app.listen(PORT,() => console.log(`hosting @${PORT}`));
